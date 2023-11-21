2c4c5653b18dc8fe84bf9779561c6876b74da7a2
88Robert <robert.magnusson1988@gmail.com>
2023-11-21 14:40:35 +0100

    firts commit

---
+++ server.js
@@ -0,0 +1,56 @@
+let express = require("express");
+let app = express();
+let port = 8080;
+
+let httpServer = app.listen(port, function () {
+    console.log(`Webbservern körs på port ${port}`);
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+let fs = require("fs");
+
+app.get("/form", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/", function(req, res) {
+    let inlagg = JSON.parse(fs.readFileSync("inlagg.json").toString());
+
+    let output = "";
+    for (let i = 0; i < inlagg.length; i++) {
+        output += `<p><b>${inlagg[i].author}</b> från ${inlagg[i].from} skriver: <br> ${inlagg[i].message} </p>`;
+        }
+    let html = fs.readFileSync("index.html").toString();
+    html = html.replace("***GÄSTER***", output);
+    res.send(html);
+});
+
+app.post("/", function (req, res) {
+    let inlagg = fs.readFileSync("inlagg.json").toString();
+    inlagg = JSON.parse(inlagg);
+
+    console.log(req.body);
+    inlagg.push(req.body);
+
+    let jsonText = JSON.stringify(inlagg);
+
+    fs.writeFileSync("inlagg.json", jsonText);
+
+    let output = "";
+    for (let i = 0; i < inlagg.length; i++) {
+      output += `<p><b>${inlagg[i].author}</b> från ${inlagg[i].from} skriver: <br> ${inlagg[i].message} </p>`;
+    }
+    let html = fs.readFileSync("index.html").toString();
+    html = html.replace("***GÄSTER***", output);
+    res.send(html);
+    location.reload();
+});
+
+
+
+
+
+
+
+
