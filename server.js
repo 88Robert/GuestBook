f7b91838d49faf1f3ba33534f27a6bc5c75cad1d
88Robert <robert.magnusson1988@gmail.com>
2023-11-24 11:38:02 +0100

    forum

--- server.js
+++ server.js
@@ -59,3 +59,101 @@ app.get("/loggin", function(req,res){
 
 
 
+/* et con = mysql.createConnection({
+    host: "localhost", // IP-adress till databas-servern
+    user: "root", // standard-användarnamn till XAMPPs databas
+    password: "", // standardlösenord
+    database: "niklasforum", // ÄNDRA TILL NAMN PÅ DIN DATABAS
+  });
+  
+  app.use(session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+  }));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static(path.join(__dirname, 'static')));
+  
+  app.get('/', function(request, response) {
+      // Render login template
+      response.sendFile(path.join(__dirname + '/login.html'));
+  });
+ */
+/* let con = mysql.createConnection({
+    host: "localhost", // IP-adress till databas-servern
+    user: "root", // standard-användarnamn till XAMPPs databas
+    password: "", // standardlösenord
+    database: "niklasforum", // ÄNDRA TILL NAMN PÅ DIN DATABAS
+  });
+  
+  app.use(session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+  }));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static(path.join(__dirname, 'static')));
+  
+  app.get('/', function(request, response) {
+      // Render login template
+      response.sendFile(path.join(__dirname + '/login.html'));
+  });
+  
+  
+  app.post('/checklogin', function(request, response) {
+      // Capture the input fields
+      let username = request.body.username;
+      let password = request.body.password;
+      // Ensure the input fields exists and are not empty
+      if (username && password) {
+          // Execute SQL query that'll select the account from the database based on the specified username and password
+          con.query('SELECT * FROM niklasforum WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+              // If there is an issue with the query, output the error
+              if (error) throw error;
+              // If the account exists
+              if (results.length > 0) {
+                  // Authenticate the user
+                  request.session.loggedin = true;
+                  request.session.username = username;
+                  // Redirect to home page
+                  response.redirect('/main');
+              } else {
+                  response.send('Incorrect Username and/or Password!');
+              }			
+              response.end();
+          });
+      } else {
+          response.send('Please enter Username and Password!');
+          response.end();
+      }
+  });
+  
+  app.get('/login', function(request, response) {
+      // If the user is loggedin
+      if (request.session.loggedin) {
+          // Output username
+      response.send('Welcome back, ' + request.session.username + '!');
+      response.redirect('/main');
+      
+      
+    
+      } else {
+          // Not logged in
+          response.send('Please login to view this page!');
+      }
+      response.end();
+  });
+  
+  
+  
+  exports.logout = (req,res) => {
+    res.clearCookie('jwt');
+    res.redirect('/login');
+  };
+ */
+
+
+
+
